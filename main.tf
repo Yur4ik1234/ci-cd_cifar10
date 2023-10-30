a25cf156cdccef167eda0dba3ee082221030375f
Yur4ik12345 <gajdaryura@gmail.com>
2023-10-30 23:57:43 +0200

    check new branch

--- main.tf
+++ main.tf
@@ -171,7 +171,7 @@ storage_profile_image_reference {
    os_profile {
     computer_name_prefix = "testvm"
     admin_username       = "adminuser"
-    #admin_password       = "Yura111!"
+    admin_password       = "Yura111!"
    }
 
    os_profile_linux_config {
